f9547db5c8c0052e893ebfcb7905121e75595d08
mgolshann <m_golshan85@yahoo.com>
2021-06-30 10:47:52 -0700

    implementing api token in gql with jwt

--- models/user.js
+++ models/user.js
@@ -1,33 +1,38 @@
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate');
 const bcrypt = require('bcrypt');
-
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
-    name : { type : String , required : true },
-    age : { type : Number , required : true },
-    address : { type : String , required : true},
-    admin : { type : Boolean ,  default : 0 },
-    email : { type : String , unique : true , required : true},
-    password : { type : String ,  required : true },
-} , { timestamps : true });
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    address: { type: String, required: true },
+    admin: { type: Boolean, default: 0 },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+}, { timestamps: true });
 
 userSchema.plugin(mongoosePaginate);
 
-userSchema.statics.hashPassword = function(password) {
+userSchema.statics.hashPassword = function (password) {
     let salt = bcrypt.genSaltSync(15);
     let hash = bcrypt.hashSync(password, salt);
     return hash;
 }
 
-userSchema.methods.comparePassword = function(password) {
+userSchema.statics.createToken = async (user, secret, expiresIn) => {
+    const { id, email } = user;
+    return await jwt.sign({ id, email }, secret, { expiresIn });
+}
+
+userSchema.methods.comparePassword = function (password) {
     return bcrypt.compareSync(password, this.password)
 }
 
-userSchema.virtual('articles' , {
-    ref : 'Article',
-    localField : '_id',
-    foreignField : 'user'
+userSchema.virtual('articles', {
+    ref: 'Article',
+    localField: '_id',
+    foreignField: 'user'
 })
 
-module.exports = mongoose.model('User' , userSchema);
+module.exports = mongoose.model('User', userSchema);
